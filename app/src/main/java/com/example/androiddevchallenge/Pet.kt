e21f8dd2f49f6dc87c12cfb6883ebccdc5f6b84c
matejmecka <matej.plavevski+github@gmail.com>
2021-02-24 19:45:12 +0100

    nesh

---
+++ app/src/main/java/com/example/androiddevchallenge/Pet.kt
@@ -0,0 +1,25 @@
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.Column
+
+@Composable
+fun Pet(){
+    Column {
+        Text("PET NAME")
+        Text("Border")
+        Text("Location")
+    }
+
+    Column {
+        Text("Gender")
+        Text("Age")
+    }
+
+    Column {
+        Text("Describe")
+        Text("Button")
+    }
+
+
+}
