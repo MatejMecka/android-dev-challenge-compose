7bb39ad4aa67e2f61d1c4198f015a1ea659ffc1e
matejmecka <matej.plavevski+github@gmail.com>
2021-02-25 13:32:34 +0100

    spotless

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -29,6 +29,7 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.onClick
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,7 +38,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.compose.ui.semantics.onClick
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 @Composable
